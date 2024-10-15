1b0864db949b2897641e684de3f97a92c8b53662
nicolas-leb <nicolas.lebrun@posteo.net>
2024-10-15 20:34:01 +0200

    start creating a new template

--- PromptBuilder.Desktop/ViewModels/CreateTemplateViewModel.cs
+++ PromptBuilder.Desktop/ViewModels/CreateTemplateViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,4 +11,24 @@ namespace PromptBuilder.Desktop.ViewModels;
 
 internal partial class CreateTemplateViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string templateBody = "Template body.";
+
+    [ObservableProperty]
+    private string newVariableName = string.Empty;
+
+    [ObservableProperty]
+    private ObservableCollection<string> templateVariables = new ();
+
+    [RelayCommand]
+    private void AddNewVariable()
+    {
+        if (this.TemplateVariables.Contains(this.NewVariableName))
+        {
+            return;
+        }
+
+        this.TemplateVariables.Add(this.NewVariableName);
+        this.NewVariableName = string.Empty;
+    }
 }
